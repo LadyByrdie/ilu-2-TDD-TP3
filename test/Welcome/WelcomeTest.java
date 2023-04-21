5803339199a9ac799eb21b400ad8c2e3c3eb36ce
LadyByrdie <eladden502@gmail.com>
2023-04-21 14:16:08 +0200

    version Initial

---
+++ test/Welcome/WelcomeTest.java
@@ -0,0 +1,14 @@
+package Welcome;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class WelcomeTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
